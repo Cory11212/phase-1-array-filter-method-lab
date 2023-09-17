085fcd5032e4274b5b45999d149f4b741cf05134
Cory <corymclean0@gmail.com>
2023-09-17 17:25:48 -0400

    good

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(array, string){
+    let goo = array.filter(function(x){
+        return x.toUpperCase() === string.toUpperCase()
+    })
+  return goo
+}
+
+function fuzzyMatch(array, string){
+    let foo = array.filter(function(x){
+        return x[0].toUpperCase() === string[0].toUpperCase()
+    })
+  return foo
+}
+
+function matchName(array, string){
+    let eoo = array.filter(function(x){
+        return x.name.toUpperCase() === string.toUpperCase()
+    })
+  return eoo
+}
